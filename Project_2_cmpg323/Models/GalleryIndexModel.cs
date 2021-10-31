200eea41c6b5ca5072fa260764bee9f1688a330d
Sven Beyleveld <minecraftsven@gmail.com>
2021-10-31 16:34:39 +0200

    Add project files.

---
+++ Project_2_cmpg323/Models/GalleryIndexModel.cs
@@ -0,0 +1,11 @@
+﻿using SimpleImageGallery.Data.Models;
+using System.Collections.Generic;
+
+namespace Project_2_cmpg323.Models
+{
+    public class GalleryIndexModel
+    {
+        public IEnumerable<GalleryImage> Images { get; set; }
+        public string SearchQuery { get; set; }
+    }
+}
---
+++ SimpleImageGallery.Services/ImageService.cs
@@ -0,0 +1,25 @@
+﻿using SimpleImageGallery.Data;
+using SimpleImageGallery.Data.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SimpleImageGallery.Services
+{
+    public class ImageService : IImage
+    {
+        public IEnumerable<GalleryImage> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public GalleryImage GetById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<GalleryImage> GetWithTag(string tag)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
