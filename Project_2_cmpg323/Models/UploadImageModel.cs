a2498007fc9bd5d4a7fd7ca1bf3aef55300c7990
Sven Beyleveld <minecraftsven@gmail.com>
2021-11-01 15:50:39 +0200

    Commit_6

---
+++ Project_2_cmpg323/Models/UploadImageModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Project_2_cmpg323.Models
+{
+    public class UploadImageModel
+    {
+        public string Title { get; set; }
+        public string Tags { get; set; }
+        public IFormFile ImageUpload { get; set; }
+    }
+}
